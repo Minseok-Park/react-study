dc61b6549231cf705f4032efbc697d6307f4d947
minseok-Park <pms0905@naver.com>
2021-10-01 17:06:55 +0900

    spread 문법 활용하여 불변성 지키면서 state update하기

--- react-begin/src/app.jsx
+++ react-begin/src/app.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./app.css";
-import Counter from "./components/counter";
 import InputSample from "./components/inputSample";
 
 function App() {
--- react-begin/src/components/inputSample.jsx
+++ react-begin/src/components/inputSample.jsx
@@ -2,24 +2,47 @@ import React from "react";
 import { useState } from "react/cjs/react.development";
 
 const InputSample = (props) => {
-  const [text, setText] = useState("");
+  const [info, setInfo] = useState({
+    name: "",
+    nickname: "",
+  });
+
+  const { name, nickname } = info;
 
   const onChange = (event) => {
-    setText((text) => event.target.value);
+    const { name, value } = event.target;
+    setInfo((info) => ({
+      ...info,
+      [name]: value,
+    }));
   };
 
   const onReset = () => {
-    setText("");
+    setInfo({
+      name: "",
+      nickname: "",
+    });
   };
 
   return (
     <div>
-      {/*value에 값을 넣어줘야 초기화가 된다*/}
-      <input type="text" onChange={onChange} value={text} />
+      <input
+        name="name"
+        type="text"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+      />
+      <input
+        name="nickname"
+        type="text"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값 : </b>
-        {text}
+        <b>값 : </b> {name}({nickname})
       </div>
     </div>
   );
