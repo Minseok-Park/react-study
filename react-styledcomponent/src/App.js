fe6a04b9017520e73bd48cef61d14b415c660d2d
minseok-Park <pms0905@naver.com>
2021-10-24 16:24:31 +0900

    styled component 활용하여 size, color 등등 속성 구현

--- react-styledcomponent/src/App.js
+++ react-styledcomponent/src/App.js
@@ -1,25 +1,64 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import Button from "./components/button";
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${(props) => props.color};
-  border-radius: 50%;
-  ${(props) =>
-    props.huge &&
-    css`
-      width: 10rem;
-      height: 10rem;
-    `}
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
 `;
+
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+const palette = {
+  blue: "#228be6",
+  gray: "#496057",
+  pink: "#f06595",
+};
 
 function App() {
   return (
-    <>
-      <Circle color="black" />
-      <Circle color="blue" huge />
-    </>
+    <ThemeProvider theme={{ palette }}>
+      <AppBlock>
+        <ButtonGroup>
+          <Button size="large">BUTTON</Button>
+          <Button color="gray">BUTTON</Button>
+          <Button size="small" color="pink">
+            BUTTON
+          </Button>
+        </ButtonGroup>
+
+        <ButtonGroup>
+          <Button size="large" outline>
+            BUTTON
+          </Button>
+          <Button color="gray" outline>
+            BUTTON
+          </Button>
+          <Button size="small" color="pink" outline>
+            BUTTON
+          </Button>
+        </ButtonGroup>
+
+        <ButtonGroup>
+          <Button size="large" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="gray" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="pink" fullWidth>
+            BUTTON
+          </Button>
+        </ButtonGroup>
+      </AppBlock>
+    </ThemeProvider>
   );
 }
 
