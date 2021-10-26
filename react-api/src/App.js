2cfc515a8e5d125ee7c2b266a888edec20d6bf9f
minseok-Park <pms0905@naver.com>
2021-10-26 13:09:46 +0900

    Context에서 비동기 상태 관리

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import Users from "./components/users";
+import { UsersProvider } from "./components/usersContext";
 import UsersRedu from "./components/users_redu";
 
 function App() {
-  return <UsersRedu />;
+  return (
+    <UsersProvider>
+      <UsersRedu />
+    </UsersProvider>
+  );
 }
 
 export default App;
