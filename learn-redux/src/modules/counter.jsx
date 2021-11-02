164d529dbd82a23d2ad7b9bd3e64f10b3d52cf16
minseok-Park <pms0905@naver.com>
2021-11-02 16:32:47 +0900

    container component & prentaional component 이용해 보기

--- learn-redux/src/modules/counter.jsx
+++ learn-redux/src/modules/counter.jsx
@@ -3,8 +3,8 @@ const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 export const setDiff = (diff) => ({ type: SET_DIFF, diff });
-export const setIncrease = () => ({ type: INCREASE });
-export const setDecrease = () => ({ type: DECREASE });
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
 
 const initialState = {
   number: 0,
