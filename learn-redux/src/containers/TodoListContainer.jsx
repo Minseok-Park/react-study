52160f249ceaeb28e8d1ea0eb23f681cbe6435d2
minseok-Park <pms0905@naver.com>
2021-11-09 11:37:46 +0900

    redux ducks-pattern 복습

--- learn-redux/src/containers/TodoListContainer.jsx
+++ learn-redux/src/containers/TodoListContainer.jsx
@@ -1,8 +1,13 @@
-import { useSelector } from "react-redux";
+import { shallowEqual, useSelector } from "react-redux";
 import TodoList from "../components/todoList";
 
 function TodoListContainer() {
-  const todos = useSelector((state) => state.todos);
+  const { todos } = useSelector(
+    (state) => ({
+      todos: state.todos,
+    }),
+    shallowEqual
+  );
 
   return <TodoList todos={todos} />;
 }
